9ae8d0a5c5adf60daac9134f3efdbe23c1560671
Zakariae <zakariaelhichem@gmail.com>
2022-06-24 21:17:17 +0000

    Add Decorator design pattern

---
+++ src/main/java/org/designpatterns/structurals/decorator/SimpleWindow.java
@@ -0,0 +1,15 @@
+package org.designpatterns.structurals.decorator;
+
+/**
+ * Window implementation
+ *
+ * Concrete implementation
+ */
+public class SimpleWindow implements Window {
+
+    @Override
+    public void renderWindow() {
+        // implementation of rendering details
+
+    }
+}
---
+++ src/main/java/org/designpatterns/structurals/decorator/Window.java
@@ -0,0 +1,12 @@
+package org.designpatterns.structurals.decorator;
+
+/**
+ * Window Interface
+ *
+ * Component window
+ */
+public interface Window {
+
+    public void renderWindow();
+
+}
