cee229d13027586efdd01defc200fee9794f1f8a
Zakariae <zakariaelhichem@gmail.com>
2022-06-24 21:21:44 +0000

    Complete Decorator pattern implementation

---
+++ src/main/java/org/designpatterns/structurals/decorator/DecoratedWindow.java
@@ -0,0 +1,21 @@
+package org.designpatterns.structurals.decorator;
+
+public class DecoratedWindow implements Window{
+
+    /**
+     * private reference to the window being decorated
+     */
+    private Window privateWindowRefernce = null;
+
+    public DecoratedWindow( Window windowRefernce) {
+
+        this.privateWindowRefernce = windowRefernce;
+    }
+
+    @Override
+    public void renderWindow() {
+
+        privateWindowRefernce.renderWindow();
+
+    }
+}
